9d64d7051441af0ce2af0dde7f7cf64e8d6d7dbe
ish013 <ishpuliyani@gmail.com>
2025-01-07 13:40:17 +0530

    Initial Commit:

---
+++ Binary.java
@@ -0,0 +1,6 @@
+
+public class Binary {
+    public static void main(String[] args) {
+        System.out.println("Hello WOrld");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Java-programming
 this file contain all dsa solutions in java
+which cna be helpful for others.
